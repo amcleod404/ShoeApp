28fcad1d3a7c17a46ec0b1d9cb8d5369dae86dda
amcleod404 <ammcleod2000@gmail.com>
2020-06-27 10:59:02 -0400

    oh yea noseplug

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
 // UI Elements
-const container = document.querySelector('#media-grid');
-const selectAFolder = document.querySelector('#body-text');
-const byStylePage = document.querySelector('#by-style-page');
-const byColorPage = document.querySelector('#by-color-page');
+const container = document.querySelector("#media-grid");
+const selectAFolder = document.querySelector("#body-text");
+const byStylePage = document.querySelector("#by-style-page");
+const byColorPage = document.querySelector("#by-color-page");
 const bySpecialRequirementsPage = document.querySelector(
-  '#by-special-requirements-page'
+  "#by-special-requirements-page"
 );
+const byPricePage = document.querySelector("#by-price-page");
+const byFitPage = document.querySelector("#by-fit-page");
+const byArchTypePage = document.querySelector("#by-arch-type-page");
 
 // UI Animation
-container.addEventListener('click', (e) => {
-  if (e.target.id !== 'media-grid') {
-    if (e.target.id === 'by-style') {
-      byStylePage.style.display = 'flex';
+container.addEventListener("click", (e) => {
+  if (e.target.id !== "media-grid") {
+    if (e.target.id === "by-style") {
+      byStylePage.style.display = "flex";
       loadStylePage(byStylePage);
-    } else if (e.target.id === 'by-color') {
-      byColorPage.style.display = 'flex';
-    } else if (e.target.id === 'by-special-requirements') {
-      bySpecialRequirementsPage.style.display = 'flex';
+    } else if (e.target.id === "by-color") {
+      byColorPage.style.display = "flex";
+    } else if (e.target.id === "by-special-requirements") {
+      bySpecialRequirementsPage.style.display = "flex";
+    } else if (e.target.id === "by-price") {
+      byPricePage.style.display = "flex";
+    } else if (e.target.id === "by-fit") {
+      byFitPage.style.display = "flex";
+    } else if (e.target.id === "by-arch-type") {
+      byArchTypePage.style.display = "flex";
     }
-    container.style.display = 'none';
-    selectAFolder.style.display = 'none';
+    container.style.display = "none";
+    selectAFolder.style.display = "none";
   }
 });
 
 // Functions
 const loadStylePage = async (page) => {
-  const res = await fetch('db.json');
+  const res = await fetch("db.json");
   const data = await res.json();
   page.children[1].innerHTML = `
     <h4>${data.defaultShoeId.name}</h4>
