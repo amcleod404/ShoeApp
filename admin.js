5c017e76f077a8880be4b10d76afdb383d6efa5d
amcleod404 <ammcleod2000@gmail.com>
2020-07-02 15:24:39 -0400

    Front end done

--- admin.js
+++ admin.js
@@ -1,5 +1,3 @@
-const submitButton = document.querySelector("#submit");
-
 const verification = (name, pass) => {
   if (String(name) === "test" && String(pass) === "noseplug") {
     window.location.href = "inventory.html";
